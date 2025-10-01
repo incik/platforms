e257750047a4477749c9fdfabcf2ee215af8b6ad
Tomáš Vaisar <tomas@vaisar.cz>
2025-10-01 11:45:09 +0200

    Working compiled picolo server addon for reference

--- public/addons-built/picolo.server.js
+++ public/addons-built/picolo.server.js
@@ -42,7 +42,7 @@
     }
 
     // scripts/jsx-json-factory.js
-    function h(type, props, ...children) {
+    async function h(type, props, ...children) {
       const flatChildren = children
         .flat(Infinity)
         .filter((c) => c !== void 0 && c !== null);
@@ -73,24 +73,28 @@
     };
 
     // addons/picolo/greeter.server.jsx
-    var User = ({ name, username, email, address }) => {
-      return /* @__PURE__ */ h(
+    var User = async ({ name, username, email, address }) => {
+      return /* @__PURE__ */ await h(
         "div",
         { className: "user-item" },
-        /* @__PURE__ */ h("h2", { className: "text-xl font-semibold" }, name),
-        /* @__PURE__ */ h(
+        /* @__PURE__ */ await h(
+          "h2",
+          { className: "text-xl font-semibold" },
+          name
+        ),
+        /* @__PURE__ */ await h(
           "p",
           { className: "text-sm text-gray-600" },
           "Username: ",
           username
         ),
-        /* @__PURE__ */ h(
+        /* @__PURE__ */ await h(
           "p",
           { className: "text-sm text-gray-600" },
           "Email: ",
           email
         ),
-        /* @__PURE__ */ h(
+        /* @__PURE__ */ await h(
           "p",
           { className: "text-sm text-gray-600" },
           "Address: ",
@@ -101,47 +105,50 @@
       );
     };
     var Caputo = async () => {
-      const URL = "https://jsonplaceholder.typicode.com/userss/";
+      const URL = "https://jsonplaceholder.typicode.com/users/";
       const data = await fetch(URL);
       log("Fetched data", data);
-      return /* @__PURE__ */ h(
+      return /* @__PURE__ */ await h(
         "div",
         { className: "client-addon" },
-        /* @__PURE__ */ h("h2", { style: { color: "green" } }, "Caputo"),
+        /* @__PURE__ */ await h("h2", { style: { color: "green" } }, "Caputo"),
         Object.keys(data).length > 0
-          ? /* @__PURE__ */ h(
+          ? /* @__PURE__ */ await h(
               Fragment,
               null,
-              /* @__PURE__ */ h(
+              /* @__PURE__ */ await h(
                 "h2",
                 { className: "text-xl font-semibold mt-4" },
                 "Users:"
               ),
-              /* @__PURE__ */ h(
+              /* @__PURE__ */ await h(
                 "div",
                 {
                   className:
                     "flex flex-col gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4",
                 },
-                data.map((user) =>
-                  /* @__PURE__ */ h(User, {
-                    key: user.id,
-                    name: user.name,
-                    username: user.username,
-                    email: user.email,
-                    address: user.address,
-                  })
+                await Promise.all(
+                  data.map(
+                    async (user) =>
+                      /* @__PURE__ */ await h(User, {
+                        key: user.id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email,
+                        address: user.address,
+                      })
+                  )
                 )
               ),
               " "
             )
-          : /* @__PURE__ */ h(
+          : /* @__PURE__ */ await h(
               "div",
               {
                 className:
                   "error border border-red-500 p-4 bg-red-100 text-red-700",
               },
-              /* @__PURE__ */ h("p", null, "No data available")
+              /* @__PURE__ */ await h("p", null, "No data available")
             )
       );
     };
@@ -154,16 +161,16 @@
       log("Foo prop:", foo);
       try {
         try {
-          return /* @__PURE__ */ h(
+          return /* @__PURE__ */ await h(
             "div",
             null,
-            /* @__PURE__ */ h(Caputo, null),
-            /* @__PURE__ */ h(
+            /* @__PURE__ */ await h(Caputo, null),
+            /* @__PURE__ */ await h(
               "h1",
               { className: "text-2xl font-bold" },
               product.name
             ),
-            /* @__PURE__ */ h(
+            /* @__PURE__ */ await h(
               "p",
               { className: "text-lg" },
               "Price: $",
@@ -172,13 +179,13 @@
           );
         } catch (e) {
           error("Error rendering data:", e.message);
-          return /* @__PURE__ */ h(
+          return /* @__PURE__ */ await h(
             "div",
             {
               className:
                 "error border border-red-500 p-4 bg-red-100 text-red-700",
             },
-            /* @__PURE__ */ h("p", null, "Server error: ", e.message)
+            /* @__PURE__ */ await h("p", null, "Server error: ", e.message)
           );
         }
       } catch (e) {
